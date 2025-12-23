020136e2c42bd59ef6050097724b77fc22fd5b23
Atulya <jaiswalatulya741@gmail.com>
2025-12-23 10:10:33 +0530

    photos uploaded

--- litfest/src/components/Gallery/Gallery.jsx
+++ litfest/src/components/Gallery/Gallery.jsx
@@ -1,14 +1,47 @@
 import React from "react";
 import "./Gallery.css";
 
-
+const facultyData = [
+  {
+    id: 1,
+    photo: "/Chetna.png",
+    name: "Dr. Chetna Sharma Rajput",
+    designation: "Dept of HSS, Faculty In-charge",
+  },
+  {
+    id: 2,
+    photo: "/Sanjay.jpg",
+    name: "Dr. Sanjay Kumar",
+    designation: "Dept of IT, Professor In-charge",
+  },
+  {
+    id: 3,
+    photo: "Vijay.jpg",
+    name: "Dr. Y. Vijay Babu",
+    designation: "Dept of HSS, Faculty In-charge",
+  }
+];
 
 
 const Gallery = () => {
   return (
     <section id="gallery" className="gallery">
       <h2>Gallery</h2>
-      <p>Photos coming soon.</p>
+      <h3>Our Faculty</h3>
+      <div className="faculty">
+        
+        {facultyData.map((faculty) => (
+          <div key={faculty.id} className="gallery-card">
+            <img
+              src={faculty.photo}
+              alt={faculty.name}
+              className="faculty-img"
+            />
+            <h3>{faculty.name}</h3>
+            <p>{faculty.designation}</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
