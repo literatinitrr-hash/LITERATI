c927db2e75dd214d5c98297b6ba43b34e0086281
ash-heinz <ashwast.gupta@gmail.com>
2025-12-25 03:34:17 +0530

    Update Header.jsx
    code for updated litfest logo (header)

--- litfest/src/components/Header/Header.jsx
+++ litfest/src/components/Header/Header.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
+import logo from "../../assets/logo.svg";
 
-
-const Header = ({onRegisterClick}) => {
+const Header = ({ onRegisterClick }) => {
   return (
     <header className="header">
-      <Link to="/" className="logo">LitFest 2026</Link>
+      <Link to="/" className="logo">
+        <img src={logo} alt="Litfest 2026" />
+      </Link>
+
       <nav className="nav">
         <a href="#about">About</a>
         <a href="#events">Events</a>
         <a href="#speakers">Speakers</a>
         <a href="#timeline">Timeline</a>
-        <a  className="register-btn" onClick={onRegisterClick}>Register</a>
+        <a className="register-btn" onClick={onRegisterClick}>Register</a>
       </nav>
     </header>
   );
